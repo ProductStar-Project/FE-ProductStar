6da74bf332d68ee906e53f350a3b18da6159b2d2
doiquangtien <qangtien27999@gmail.com>
2022-07-15 17:18:15 +0700

    tempate-fb-login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import FacebookLogin from "react-facebook-login";
+import axios from "axios";
 function App() {
+  const responseFacebook = async (response: any) => {
+    try {
+      const { accessToken, userID } = response;
+      const res = await axios.post(
+        "http://localhost:5000/auth/facebook_login",
+        { accessToken: accessToken, userID: userID }
+      );
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +23,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <FacebookLogin
+          appId="2213403288836121"
+          autoLoad={false}
+          fields="name,email,picture"
+          callback={responseFacebook}
+        />
+        ,
         <a
           className="App-link"
           href="https://reactjs.org"
